0a0723f9ea478c5338ba695154e888981bda2077
Esin Çağla KIRAL <68962573+esincaglakiral@users.noreply.github.com>
2023-09-13 03:05:00 +0300

    Update easy.js

--- easy.js
+++ easy.js
@@ -1,8 +1,9 @@
 import { userValidation } from "./UserValidation Challange/userValidation.js";
 import { longestWord } from "./LongestWord Challange/longestWord.js";
 import { firstFactorial } from "./FirstFactorial Challange/firstFactorial.js";
-import {firstReverse} from "./First Reverse Challange/firstReverse.js";
-
+import { firstReverse } from "./First Reverse Challange/firstReverse.js";
+import { findIntersection } from "./Find Intersection Challange/findIntersection.js";
+import { findIntersection2 } from "./Find Intersection Challange/findIntersection2.js";
 // console.log(userValidation("u_hello_world123"));
 
 // console.log(longestWord("fun&!! time"));
@@ -10,3 +11,8 @@ import {firstReverse} from "./First Reverse Challange/firstReverse.js";
 // console.log(firstFactorial(8));
 
 // console.log(firstReverse("merhaba"));
+
+// console.log(findIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"])); // Sonucu "1,4,13" olarak döndürecek.
+// console.log(findIntersection(["1, 2, 3, 4, 5", "6, 7, 8, 9, 10"])); // Sonucu -1 olarak döndürecek.
+
+// console.log(findIntersection2(["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]));
