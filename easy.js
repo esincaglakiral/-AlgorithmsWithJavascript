03a7bb4629ec3820f78280879aa2fe9a9741e22a
Esin Çağla KIRAL <68962573+esincaglakiral@users.noreply.github.com>
2023-10-06 02:00:05 +0300

    Update easy.js

--- easy.js
+++ easy.js
@@ -5,17 +5,23 @@ import { firstReverse } from "./First Reverse Challange/firstReverse.js";
 import { findIntersection } from "./Find Intersection Challange/findIntersection.js";
 import { findIntersection2 } from "./Find Intersection Challange/findIntersection2.js";
 import { questionMark2 } from "./Question Marks Challange/questionMarks2.js";
+import { letterCapitalize } from "./LetterCapitalize Challange/letterCapitalize.js";
+import { letterChange } from "./LetterChange Challange/letterChange.js";
+import { primeTime } from "./PrimeTime Challange/primeTime.js";
+import { runLength } from "./Run Length Challange/runLength.js";
+
 // console.log(userValidation("u_hello_world123"));
-
 // console.log(longestWord("fun&!! time"));
-
 // console.log(firstFactorial(8));
-
 // console.log(firstReverse("merhaba"));
-
 // console.log(findIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"])); // Sonucu "1,4,13" olarak döndürecek.
 // console.log(findIntersection(["1, 2, 3, 4, 5", "6, 7, 8, 9, 10"])); // Sonucu -1 olarak döndürecek.
-
 // console.log(findIntersection2(["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]));
+// console.log(questionMark2("acc?7??sss?3rr1??????5"));
 
-console.log(questionMark2("acc?7??sss?3rr1??????5"));
+// console.log(letterCapitalize("bugün önce kodlama çalıştım, ardından spor yaptım, daha sonra ise bilgisayar oyunu oynadım."));
+// console.log(letterChange("Hello"));
+
+// console.log(primeTime(41332));
+
+console.log(runLength("wwwwggggoooyyypp"));
